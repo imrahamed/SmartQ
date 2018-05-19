cb54920827c8d93ff02b18369c065f99d0c640e0
Imran Ahamed <imrahamed@gmail.com>
2018-05-19 16:19:33 +0530

    css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  oriental = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
+  oriental = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"veg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
   cc = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
   cs = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
   salads = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
-  
+
   ngOnInit() {
 
   }
