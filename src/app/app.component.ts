c90df3548bc7ad0135404039aadb866b8216dd6c
Imran Ahamed <imrahamed@gmail.com>
2018-05-19 15:31:26 +0530

    UI done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  oriental = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
+  cc = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
+  cs = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
+  salads = [{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"","name":"Egg Chinese Combo","price":100,"vegflag":"nonveg"},{"availabletime":"11:00-15:30","category":"Chinese Combos","description":"Chilli Garlic Chicken, Vegetable Fried Rice/ Noodles, Hong Kong Sauce","name":"Chicken Chinese Combo","price":110,"vegflag":"nonveg"}]
+  
+  ngOnInit() {
+
+  }
 }
